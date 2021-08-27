1659166e38f07abeab0a0860accbad2184e9d50c
suhail kk <suhailmlp125@gmail.com>
2021-08-27 21:26:42 +0530

    views-family,personal

---
+++ app/src/main/java/com/example/emea/Activity/PersonalDetailView.java
@@ -0,0 +1,14 @@
+package com.example.emea.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonalDetailView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_detail_view);
+    }
+}
